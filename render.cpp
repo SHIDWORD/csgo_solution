bdee562c766afe26a677e2ffed9f1820f64a9b60
hypnotic <hypnoticproject@protonmail.com>
2022-11-07 22:08:59 -0700

    init.

--- render.cpp
+++ render.cpp
@@ -7,7 +7,7 @@ void render_t::init ( ) {
     interfaces.m_engine->get_screen_size ( m_width, m_height );
 	
     /* init font map */
-	fonts [ fonts_t::hack_watermark ] = font_data_t ( XOR ( "Verdana" ), 12, 400, 0, font_flags_t::font_flag_anti_aliasing | font_flags_t::font_flag_drop_shadow );
+    fonts [ fonts_t::hack_watermark ] = font_data_t ( XOR ( "Verdana" ), 12, 400, 0, font_flags_t::font_flag_anti_aliasing | font_flags_t::font_flag_drop_shadow );
 
     /* create all fonts */
     for ( auto &font : fonts ) {
