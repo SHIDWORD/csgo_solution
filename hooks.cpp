9ba142390e4219a03ba582c9eb367b8cb8e880e0
hypnotic <hypnoticproject@protonmail.com>
2022-11-26 19:41:18 -0700

    removed clutter.

--- hooks.cpp
+++ hooks.cpp
@@ -7,27 +7,23 @@ std::unique_ptr < c_event_handler > event_handler = nullptr;
 bool hooks_t::init ( ) {
 	m_hwnd = LI_FN ( FindWindowA )( x_ ( "Valve001" ), nullptr );
 
-	/* wndproc hook. */
 	m_old_wndproc = reinterpret_cast < WNDPROC > ( SetWindowLongPtrA ( m_hwnd, GWLP_WNDPROC, LONG_PTR ( wnd_proc ) ) );
 
 	if ( MH_Initialize ( ) != MH_OK )
 		throw std::runtime_error ( x_ ( "failed to initialize minhook." ) );
 
-	/* addresses. */
 	const auto _create_move = util::get_method < void * > ( interfaces::m_client, 22 );
 	const auto _frame_stage_notify = util::get_method < void * > ( interfaces::m_client, 37 );
 	const auto _paint = pattern::find ( x_ ( "engine.dll" ), x_ ( "55 8B EC 83 EC 40 53 8B D9 8B 0D ? ? ? ? 89" ) ).as< void * > ( );
 	const auto _paint_traverse = util::get_method < void * > ( interfaces::m_panel, 41 );
 	const auto _run_command = util::get_method < void * > ( interfaces::m_prediction, 19 );
 
-	/* create detours. */
 	m_create_move_proxy.create ( _create_move, create_move_proxy );
 	m_frame_stage_notify.create ( _frame_stage_notify, frame_stage_notify );
 	m_paint.create ( _paint, paint );
 	m_paint_traverse.create ( _paint_traverse, paint_traverse );
 	m_run_command.create ( _run_command, run_command );
 
-	/* initialize event handler. */
 	event_handler = std::make_unique < c_event_handler > ( );
 
 	MH_EnableHook ( MH_ALL_HOOKS );
@@ -64,7 +60,6 @@ void __stdcall hooks_t::create_move ( int seq_num, float input_sample_frame_time
 	g.m_ucmd = ucmd;
 	
 	prediction.predict ( ucmd, [ & ] {
-		/* prediction dependent code goes here. */
 	} );
 
 	verified->m_cmd = *ucmd;
@@ -72,20 +67,17 @@ void __stdcall hooks_t::create_move ( int seq_num, float input_sample_frame_time
 }
 
 void __fastcall hooks_t::frame_stage_notify ( void *ecx, void *edx, client_frame_stage_t stage ) {
-	if ( stage != frame_start )
+	if ( stage != client_frame_stage_t::frame_start )
 		g.m_stage = stage;
 
-	/* store local player. */
 	g.m_local = ( interfaces::m_engine->is_connected ( ) || interfaces::m_engine->is_in_game ( ) ) ? interfaces::m_entlist->get< player_t * > ( interfaces::m_engine->get_local_player ( ) ) : nullptr;
 
-	/* call og. */
 	hooks.m_frame_stage_notify.get_old_method< decltype ( &frame_stage_notify ) > ( )( ecx, edx, stage );
 }
 
 void __fastcall hooks_t::run_command ( void *ecx, void *edx, player_t *player, ucmd_t *ucmd, c_move_helper *move_helper ) {
 	hooks.m_run_command.get_old_method < decltype ( &run_command ) > ( )( ecx, edx, player, ucmd, move_helper );
 
-	/* set move helper pointer. */
 	interfaces::m_move_helper = move_helper;
 }
 
@@ -95,7 +87,6 @@ void __fastcall hooks_t::paint_traverse ( void *ecx, void *edx, unsigned int pan
 	hooks.m_paint_traverse.get_old_method< decltype ( &paint_traverse ) > ( )( ecx, edx, panel, force_repaint, allow_force );
 
 	if ( panel_hash == HASH ( "FocusOverlayPanel" ) ) {
-		/* store draw data here. */
 	}
 }
 
@@ -118,7 +109,6 @@ __declspec( naked ) void __fastcall hooks_t::create_move_proxy ( void *ecx, void
 void hooks_t::restore ( ) {
 	SetWindowLongPtrA ( m_hwnd, GWLP_WNDPROC, LONG_PTR ( m_old_wndproc ) );
 
-	/* restore hooks. */
 	MH_DisableHook ( MH_ALL_HOOKS );
 	MH_RemoveHook ( MH_ALL_HOOKS );
 	MH_Uninitialize ( );
